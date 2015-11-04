4422b898d4f8010e5e2734d7ca593e9aec37e001
usuario <usuario@10.22.143.9>
2015-11-04 09:32:10 +0000

    Segunda versión, lectura y escritura de ficheros, byte a byte y luego con buffer de 1024 bytes.

--- src/kata4/KATA4.java
+++ src/kata4/KATA4.java
@@ -1,19 +1,43 @@
 package kata4;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 
 public class KATA4 {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         String fileName = "C:\\Users\\usuario\\Documents";
         File file = new File(fileName);
         String[] filesArray = file.list();
-        for (String files : filesArray) {
+      //  for (String files : filesArray) {
             //System.out.println(files);
+        //}
+      //  print(file.listFiles(),"");
+        
+        
+        String from = "C:\\Users\\Public\\Videos\\Sample Videos\\Wildlife.wmv";
+        String to = "C:\\Users\\Public\\Videos\\Sample Videos\\Wildlife(1).wmv ";
+        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
+        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
+        byte[] buffer = new byte[1024];
+        Date date = new Date();
+        while (true){
+            int read = input.read(buffer);
+            if(read < 0) break;
+            output.write(buffer);
         }
-        print(file.listFiles(),"");
+        System.out.println(new Date().getTime()-date.getTime()+"msg");
+        input.close();
+        output.flush();
+        output.close();
     }
-
+    //C:\Users\Public\Videos\Sample Videos\Wildlife.wmv 
     private static void print(File[] listFiles, String ident) {
         if (listFiles == null ) return;
         for (File listFile : listFiles) {
