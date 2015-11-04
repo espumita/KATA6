8affb19be47dad5725c1bea7f2ffe0f17a8db060
usuario <usuario@10.22.143.9>
2015-11-04 10:31:13 +0000

    Tercera versión, Histograma completo con emails

--- src/kata4/KATA4.java
+++ src/kata4/KATA4.java
@@ -1,49 +1,16 @@
 package kata4;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Date;
+import java.util.ArrayList;
 
 public class KATA4 {
     
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        String fileName = "C:\\Users\\usuario\\Documents";
-        File file = new File(fileName);
-        String[] filesArray = file.list();
-      //  for (String files : filesArray) {
-            //System.out.println(files);
-        //}
-      //  print(file.listFiles(),"");
-        
-        
-        String from = "C:\\Users\\Public\\Videos\\Sample Videos\\Wildlife.wmv";
-        String to = "C:\\Users\\Public\\Videos\\Sample Videos\\Wildlife(1).wmv ";
-        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
-        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
-        byte[] buffer = new byte[1024];
-        Date date = new Date();
-        while (true){
-            int read = input.read(buffer);
-            if(read < 0) break;
-            output.write(buffer);
-        }
-        System.out.println(new Date().getTime()-date.getTime()+"msg");
-        input.close();
-        output.flush();
-        output.close();
+    public static void main(String[] args) throws IOException{
+        String fileName = "C:\\Users\\Usuario\\Documents\\NetBeansProjects\\KATA4\\data\\emails.txt";
+        ArrayList<String> listMail = MailList.read(fileName);
+        Histogram<String> histogram = MailHistogramBuilder.build(listMail);
+        HistogramDisplay histogramDisplay = new HistogramDisplay(histogram);
+        histogramDisplay.execute();
     }
-    //C:\Users\Public\Videos\Sample Videos\Wildlife.wmv 
-    private static void print(File[] listFiles, String ident) {
-        if (listFiles == null ) return;
-        for (File listFile : listFiles) {
-            System.out.println(ident+(listFile.isDirectory()? "+" : "  " )+listFile.getName());
-            if(!listFile.isDirectory() || listFile.isHidden()) continue;
-            print(listFile.listFiles(),ident+" ");
-        }
-    }
+    
 }
