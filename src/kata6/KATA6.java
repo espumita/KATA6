1ec15510875f52e99038f66f59e488f114ec09d6
usuario <usuario@10.22.143.6>
2015-12-03 10:13:49 +0000

    DOMINIOS, PESOS, GENERO from sDB to Histogram with Loader and person1  class

--- src/kata6/KATA6.java
+++ src/kata6/KATA6.java
@@ -2,15 +2,23 @@ package kata6;
 
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class KATA6 {
     
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException{
         String fileName = "C:\\Users\\Usuario\\Documents\\NetBeansProjects\\KATA6\\data\\emails.txt";
         ArrayList<Person> listMail = MailList.read(fileName);
+        
+        ArrayList<Person1> perso = PersonLoader.read();
+        
+        
+        
         //Histogram<String> histogram = MailHistogramBuilder.build(listMail);
         HistogramBuilder<Person> builder1 = new HistogramBuilder<>(listMail);
+        
+        HistogramBuilder<Person1> builder2 = new HistogramBuilder<>(perso);
         
         Histogram<String> domains = builder1.build(new Attribute<Person,String>() {
 
@@ -28,8 +36,38 @@ public class KATA6 {
             }
         });
         
-        new HistogramDisplay(domains,"DOMINIOS").execute();
-        new HistogramDisplay(letters,"CARACTER").execute();
+        
+        Histogram<String> domains1 = builder2.build(new Attribute<Person1, String>() {
+
+            @Override
+            public String get(Person1 item) {
+                return item.getMail().split("@")[1];
+            }
+        });
+        
+        Histogram<Float> pesos = builder2.build(new Attribute<Person1, Float>() {
+
+            @Override
+            public Float get(Person1 item) {
+                return item.getPeso();
+            }
+        });
+        
+        Histogram<Character> genero = builder2.build(new Attribute<Person1, Character>() {
+
+            @Override
+            public Character get(Person1 item) {
+                return item.getGenero().charAt(0);
+            }
+        });
+        
+        
+        //new HistogramDisplay(domains,"DOMINIOS").execute();
+        //new HistogramDisplay(letters,"CARACTER").execute();
+        
+        new HistogramDisplay(domains1,"DOMINIOS").execute();
+        new HistogramDisplay(pesos,"PESOS").execute();
+        new HistogramDisplay(genero,"GENERO").execute();
 
         //HistogramDisplay histogramDisplay = new HistogramDisplay(histogram);
         //histogramDisplay.execute();
